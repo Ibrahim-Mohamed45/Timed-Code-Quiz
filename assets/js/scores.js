e470528a04b6c1af3756d8bb48b60f7dfb2945f7
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-17 22:20:33 +0000

    Sort highscore list in terms of scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,18 +1,18 @@
 var highScores = document.querySelector("#highscores");
+var clearBtn = document.querySelector("#clear");
 
 var scores = [];
 var storage = JSON.parse(localStorage.getItem('highscore'));
 
 for (var i = 0; i < storage.length; i++) {
   scores.push(storage[i]);
+  scores.sort((a, b) => b.score - a.score);
 }
 
 for (var i = 0; i < scores.length; i++) {
   var player = scores[i];
-
   var li = document.createElement("li");
   li.textContent = `${player.initials} - Score: ${player.score}`;
-
   highScores.appendChild(li);
 }
 
